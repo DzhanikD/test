251c05e5f109a602eb925f9c3141a159b238b794
Dzhanik Dzanagov <deicideand@yandex.ru>
2022-10-01 16:06:27 +0300

    eleventh commit

--- js/script.js
+++ js/script.js
@@ -50,24 +50,69 @@ mobileSlider();
 
 window.addEventListener('resize', mobileSlider);
 
-const brand = document.querySelector('.button-read');
-const brandOpen = document.querySelector('.brand'); 
-const buttonShow = document.querySelector('.button-show')
-const showAll = document.querySelector('.content-block__show-all')
-showAll.addEventListener('click', function() {
-  brandOpen.classList.add('brand-open');
-  buttonShow.classList.add('button-hidden');
-  showAll.classList.add('show-all');
+
+/*сonst brandOpen = document.querySelector('.brand'); 
+const buttonShow = document.querySelector('.content-block__button--hidden');
+const showAll = document.querySelector('.content-block__button--show-all');
+const buttons = document.querySelectorAll('.button-show');
+const swiper = document.querySelector('.swiper');
+
+const buttonHiden = document.querySelector('.button-hidden')*/
+const contentBlock = document.querySelector('.content-block');
+
+function show(buttonShow) {
+  let spoiler = buttonShow.previousElementSibling;
+  spoiler.classList.remove('height');
+  let parent = buttonShow.parentElement;
+  let buttonHide = parent.querySelector('.button-hidden');
+  buttonHide.classList.remove('hidden');
+  buttonShow.classList.add('hidden');
+}
+
+contentBlock.addEventListener('click', function(event) {
+  let buttonShow = event.target.closest('.button-show');
+    if (buttonShow) {
+      show(buttonShow);
+    }
+    return;
 })
+
+function hidden(buttonHidden) {
+  let parent = buttonHidden.parentElement;
+  let buttonShow = parent.querySelector('.button-show');
+  let spoiler = buttonShow.previousElementSibling;
+  spoiler.classList.add('height');
+  buttonShow.classList.remove('hidden');
+  buttonHidden.classList.add('hidden');
+}
+
+contentBlock.addEventListener('click', function(event){
+  let buttonHidden = event.target.closest('.button-hidden');
+  if(buttonHidden) {
+    hidden(buttonHidden);
+  }
+  return;
+})
+
+/*showAll.addEventListener('click', function() {
+  brandOpen.classList.add('brand-height');
+  buttonShow.classList.add('show');
+  showAll.classList.add('hidden');
+})
+
+buttonShow.classList.add('show');
+    event.stopPropagation()
 
 buttonShow.addEventListener('click', function() {
-  brandOpen.classList.remove('brand-open');
-  buttonShow.classList.remove('button-hidden');
-  showAll.classList.remove('show-all')
-})
+  brandOpen.classList.remove('brand-height');
+  buttonShow.classList.remove('show');
+  showAll.classList.remove('hidden')
+})*/
 
 
 
+
+ 
 
 
 
